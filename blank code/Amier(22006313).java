136475fd05ebe002c4aca958a69dd15d1b6ad142
ironk-0 <farhanizamfairus@gmail.com>
2024-06-02 22:45:23 +0800

    Create Amier(22006313).java

---
+++ blank code/Amier(22006313).java
@@ -0,0 +1,73 @@
+//MuhammadAmierIrfan
+
+import java.util.ArrayList;
+import java.util.List;
+
+ public class Client {
+    private String name;
+    private String phoneNumber;
+    private List<Parcel> parcels;
+
+    public Client(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.parcels = new ArrayList<>();
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Parcel> getParcels() {
+        return parcels;
+    }
+
+    public void setParcels(List<Parcel> parcels) {
+        this.parcels = parcels;
+    }
+
+    public void addParcel(Parcel parcel) {
+        this.parcels.add(parcel);
+    }
+
+    public void removeParcel(Parcel parcel) {
+        this.parcels.remove(parcel);
+    }
+}
+
+public class Clients {
+    private List<Client> clients;
+
+    public Clients() {
+        this.clients = new ArrayList<>();
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public void addClient(Client client) {
+        this.clients.add(client);
+    }
+
+    public void removeClient(Client client) {
+        this.clients.remove(client);
+    }
+}
